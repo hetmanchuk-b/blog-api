8553e6d6bd93b2f4d0ea116633ee7e1ef54db0b9
hetmanchuk.b <hetmanchuk.kwts@gmail.com>
2025-05-25 22:54:28 +0300

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import categoriesRouter from './routes/categories';
 import postsRouter from './routes/posts';
 import commentsRouter from './routes/comments';
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app: Express = express();
 app.use(express.json());
+app.use(cors({origin: "http://localhost:3001"}));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Blog APO is running')
